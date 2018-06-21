ed57382c96f0b529395d4b5f497d114c4b392969
Seis <xiaonanln@gmail.com>
2018-06-21 08:55:37 +0800

    GameClient

---
+++ GoWorldUnity3D/GameClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace GoWorldUnity3D
+{
+    public class GameClient
+    {
+        private TcpClient tcpClient;
+
+        public string Host { get; }
+        public int Port { get; }
+
+        public GameClient(string host, int port)
+        {
+            this.Host = host;
+            this.Port = port;
+        }
+
+        public override string ToString()
+        {
+            return "GameClient<"+this.Host+":"+this.Port+">";
+        }
+
+        public void Connect()
+        {
+            this.tcpClient = new TcpClient(this.Host, this.Port);
+            Console.WriteLine("GameClient Connected: " + this);
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoWorldUnity3D;
+using System.Threading;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoWorldUnity3D.GameClient client = new GoWorldUnity3D.GameClient("ec2-13-229-128-242.ap-southeast-1.compute.amazonaws.com", 15011);
+            Console.WriteLine(client.ToString() + " created.");
+
+            while (true) {
+                client.Tick();
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Press Any Key To Quit.");
+            Console.ReadKey();
+        }
+    }
+}
