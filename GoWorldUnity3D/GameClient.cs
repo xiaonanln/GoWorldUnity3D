327a12e7259d64e3e385e2ac779307c090905d05
Seis <xiaonanln@gmail.com>
2018-06-21 11:40:25 +0800

    GameClient Connect

--- GoWorldUnity3D/GameClient.cs
+++ GoWorldUnity3D/GameClient.cs
@@ -27,17 +27,67 @@ namespace GoWorldUnity3D
         {
             this.Host = host;
             this.Port = port;
+            this.disconnectTCPClient();
+        }
+
+        public void Disconnect()
+        {
+            this.Host = "";
+            this.Port = 0;
+            this.disconnectTCPClient();
+        }
+
+        private void disconnectTCPClient()
+        {
             if (this.tcpClient != null)
             {
                 this.tcpClient.Close();
                 this.tcpClient = null;
+                this.debug("Disconnected");
             }
+        }
+
+        private void debug(string msg)
+        {
+            Console.WriteLine(this + " - " + msg);
         }
 
         public void Tick()
         {
-            if this.Host == "" {
+            if (this.Host == "")
+            {
+                this.disconnectTCPClient();
+            }
+            else
+            {
+                this.assureTCPClientConnected();
+                this.debug("Available: " + this.tcpClient.Available);
+            }
+        }
 
+        private void assureTCPClientConnected()
+        {
+            if (this.tcpClient != null)
+            {
+                return;
+            }
+
+            this.debug("Connecting ...");
+            this.tcpClient = new TcpClient();
+            this.tcpClient.NoDelay = true;
+            this.tcpClient.BeginConnect(this.Host, this.Port, this.onConnected, null);
+        }
+
+        private void onConnected(IAsyncResult ar)
+        {
+            if (this.tcpClient.Connected)
+            {
+                this.debug("Connected " + this.tcpClient.Connected);
+            }
+            else
+            {
+                this.debug("Connect Failed!");
+                this.disconnectTCPClient();
             }
         }
     }
