4b9b569976b60130203a1735b907600ba5671744
seis <xiaonanln@gmail.com>
2018-06-22 12:50:25 +0800

    rename package name to GoWorld

---
+++ GoWorldUnity3D/GoWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoWorld
+{
+    class GoWorld
+    {
+        public static GameClient GameClient = new GameClient();
+    }
+}
